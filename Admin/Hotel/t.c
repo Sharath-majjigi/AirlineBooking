99016a1306e3b88168938b250f4d80816f911164
DarthMuzammil <muzammil.inamdar98@gmail.com>
2020-11-30 01:24:21 +0530

    Test : - Read from file City name Hotel Name Hotel Price (double)
    - Reads by character and stores each character in a string until
      a delimiter is reached
    
    - Converts string to double

--- Admin/Hotel/t.c
+++ Admin/Hotel/t.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-int main(){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main()
+{
     FILE *F;
-    F = fopen("text.txt","r");
+    F = fopen("text.txt", "r");
     char cities[30][10];
     float f[30];
     int i[30];
@@ -12,12 +12,30 @@ int main(){
     char c;
     char str[100];
     int count = 0;
-    while(!feof(F)){
-        printf("%c",fgetc(F));
-        printf("%d\n",++count);
+    char *_;
+    while (!count)
+    {
+        int j = 0;
+        while((c = fgetc(F)) != ','){
+            str[j++] = c;
+        }
+        printf("%s",str);
+        j = 0;
+        while(( c = fgetc(F)) != '|'){
+            str[j++] = c;
+        }
+        printf("%s",str);
+        j = 0;
+        while(( c = fgetc(F)) != '|'){
+            str[j++] = c;
+        }
+        double r = strtod(str,&_);
+        printf("\n%lf\n",r);
+        count = 1;
     }
-   // printf("%s\n",str);
-   // printf("%c",fgetc(F));
+    printf("a%d", feof(F));
+    // printf("%s\n",str);
+    // printf("%c",fgetc(F));
     fclose(F);
     return 0;
 }
