0816e038a7963da603de00665d43b4b47df870b6
DarthMuzammil <muzammil.inamdar98@gmail.com>
2020-12-01 22:34:49 +0530

    Fixed Errors

--- Admin/Hotel/ll_windows.c
+++ Admin/Hotel/ll_windows.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
 
@@ -10,11 +10,12 @@ This will be a node and a link pointing to
 to N of these nodes will be stored in city
 
 */
-struct hotel{
+struct hotel
+{
     char name[30];
     double price;
     int rooms;
-    struct hotel* next_hotel;
+    struct hotel *next_hotel;
 };
 /* 
 
@@ -25,40 +26,49 @@ HOTELS is a link to the first of the N hotels
 for a particular city
 
 */
-struct city{
+struct city
+{
     char name[30];
-    struct hotel* HOTELS; //This of this as first for the hotels linked list
-    struct city* next_city;
+    struct hotel *HOTELS; //This of this as first for the hotels linked list
+    struct city *next_city;
 };
-struct city* City_Head; //Think of this as first for our linked list 
+struct city *City_Head; //Think of this as first for our linked list
 
 /*
 This function searches city name that if it is available is the list.
 */
-struct city *choose_city(struct city * head, char city_name [50]){
-    struct city * city_ptr = head;
-    while(city_ptr->next_city != NULL){
-        if(city_ptr->name==city_name)
+struct city *choose_city(struct city *head, char city_name[])
+{
+    struct city *city_ptr = head;
+    while (city_ptr != NULL)
+    {
+        if (!strcmp(city_name, city_ptr->name))
             return city_ptr;
-        city_ptr=city_ptr->next_city;
+        city_ptr = city_ptr->next_city;
     }
     return NULL;
 }
 
-
-
 /* this function displays the searched city name and 
 the hotels list in that city */
- 
-void displaynodecity(struct city * c)
+
+void displaynodecity(struct city *c)
 {
- struct hotel *h=c->HOTELS;
-printf("%s",c->name);
-while(h!=NULL);
-{
-printf("%s\n",h->name);
-h=h->next_hotel; 
-}
+    if (c == NULL)
+    {
+        printf("\nCITY NOT FOUND !\n");
+    }
+    else
+    {
+
+        struct hotel *h = c->HOTELS;
+        printf("\nHOTELS AVAILABLE IN %s\n",c->name);
+        while (h != NULL)
+        {
+            printf("%s %lf %d\n", h->name,h->price,h->rooms);
+            h = h->next_hotel;
+        }
+    }
 }
 
 /*
@@ -72,9 +82,10 @@ And appends a hotel type node in a FILO fashion
 
 */
 
-    //Variable Declarations
-void addhotel(struct city* C){
-    struct hotel* Hotel_Head = (struct hotel*)malloc(sizeof(struct hotel));
+//Variable Declarations
+void addhotel(struct city *C)
+{
+    struct hotel *Hotel_Head = (struct hotel *)malloc(sizeof(struct hotel));
     Hotel_Head = C->HOTELS;
     char hotelname[30];
     double hotelprice;
@@ -82,26 +93,28 @@ void addhotel(struct city* C){
 
     //Reading the input
     printf("\nEnter the Hotel name\n");
-    scanf("%s",hotelname);
+    scanf("%s", hotelname);
     printf("\nEnter the hotel price\n");
-    scanf("%lf",&hotelprice);
+    scanf("%lf", &hotelprice);
     printf("\nEnter the number of rooms\n");
-    scanf("%d",&hotelrooms);
+    scanf("%d", &hotelrooms);
 
     //A temporary node of type hotel
-    struct hotel* temp = (struct hotel*)malloc(sizeof(struct hotel));
+    struct hotel *temp = (struct hotel *)malloc(sizeof(struct hotel));
 
     //Copy the input data into the temporary node
-    strcpy(temp->name,hotelname);
+    strcpy(temp->name, hotelname);
     temp->rooms = hotelrooms;
     temp->price = hotelprice;
 
     //Check if list is empty
-    if(Hotel_Head == NULL){
+    if (Hotel_Head == NULL)
+    {
         temp->next_hotel = NULL;
         Hotel_Head = temp;
     }
-    else{
+    else
+    {
         temp->next_hotel = Hotel_Head;
         Hotel_Head = temp;
     }
@@ -112,45 +125,48 @@ void addhotel(struct city* C){
     C->HOTELS = Hotel_Head;
 }
 
-
 /*
 This function adds N hotels
 */
-void addNhotels(struct city* C,int n){
-    for(int i = 0; i < n; i++){
+void addNhotels(struct city *C, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
         addhotel(C);
     }
 }
 
-
 /* 
 This is simpler and analogus to the addhotel() function
 */
-void addcity(){
+void addcity()
+{
     //Variable Declarations
     char cityname[30];
     int number_of_hotels;
-    struct city* temp = (struct city*)malloc(sizeof(struct city));
+    struct city *temp = (struct city *)malloc(sizeof(struct city));
 
     //Read input
     printf("\nEnter city name\n");
-    scanf("%s",cityname);
-    printf("\nHow many hotels in %s\n",cityname);
-    scanf("%d",&number_of_hotels);
+    scanf("%s", cityname);
+    printf("\nHow many hotels in %s\n", cityname);
+    scanf("%d", &number_of_hotels);
 
     //Copy the data into node
-    strcpy(temp->name,cityname);
+    strcpy(temp->name, cityname);
 
     //Manage dangling pointer
     temp->HOTELS = NULL;
 
     //Check if list is empty
-    if(City_Head == NULL){
+    if (City_Head == NULL)
+    {
         temp->next_city = NULL;
         City_Head = temp;
         addNhotels(temp, number_of_hotels); //Call function for adding hotels to this city
     }
-    else{
+    else
+    {
         temp->next_city = City_Head;
         City_Head = temp;
         addNhotels(temp, number_of_hotels);
@@ -160,19 +176,21 @@ void addcity(){
 /*
 Function to display the hotels
 */
-void displayhotels(struct city* C){
+void displayhotels(struct city *C)
+{
     //Variable Declarations
-    struct hotel* Head  = C->HOTELS;
-    struct hotel* temp = Head;
+    struct hotel *Head = C->HOTELS;
+    struct hotel *temp = Head;
 
-    //Display 
+    //Display
     /*
     To Do : Shreya
     - Display data and style the output
     */
-    printf("%s %s %s\n","HOTEL NAME","PRICE","ROOMS");
-    while(temp != NULL){
-        printf("%s %lf %d\n",temp->name,temp->price,temp->rooms);
+    printf("%s %s %s\n", "HOTEL NAME", "PRICE", "ROOMS");
+    while (temp != NULL)
+    {
+        printf("%s %lf %d\n", temp->name, temp->price, temp->rooms);
         temp = temp->next_hotel;
     }
 }
@@ -180,17 +198,19 @@ void displayhotels(struct city* C){
 /*
 Function to display cities
 */
-void displaycities(){
+void displaycities()
+{
     //Variable Declarations
-    struct city* temp = City_Head;
+    struct city *temp = City_Head;
 
-    //Display 
+    //Display
     /*
     To Do : Shreya
     - Display data and style the output
     */
-    while(temp != NULL){
-        printf("%s\n\n",temp->name);
+    while (temp != NULL)
+    {
+        printf("%s\n\n", temp->name);
         printf("\n");
         displayhotels(temp);
         temp = temp->next_city;
@@ -206,14 +226,16 @@ Takes string input str and displays :
 " Do you want to add a (prinit str) "
 
 */
-void ask(char str[]){
-    printf("\nDo you want to add a %s\n(Y/N)",str);
-    scanf("%s",choose);
+void ask(char str[])
+{
+    printf("\nDo you want to add a %s\n(Y/N)", str);
+    scanf("%s", choose);
 }
 
 //Driver code
 int main()
 {
+    char whichcity[100];
     City_Head = NULL;
     ask("city");
     while (choose[0] == 'Y')
@@ -222,6 +244,10 @@ int main()
         ask("city");
     }
     displaycities();
+    printf("\nChoose a city\n");
+    scanf("%s", whichcity);
+    //printf("%s",whichcity);
+    displaynodecity(choose_city(City_Head, whichcity));
 
     return 0;
 }
