b4a20d9e0d2841bf222b639180e90a800f1c68b4
DarthMuzammil <muzammil.inamdar98@gmail.com>
2020-12-25 22:49:24 +0530

    1. Read from file into variables 2. Initialize structure from file

--- Admin/Hotel/t.c
+++ Admin/Hotel/t.c
@@ -48,28 +48,76 @@ void extractHotel(FILE *file, char str_h[90], double *p, int *r)
     *p = price;
     *r = rooms;
 }
-int main()
-{
+struct hotel* addHotel(struct hotel* Hotel_Head,char hotelname[100], double price, int rooms){
+    struct hotel* temp = (struct hotel*)malloc(sizeof(struct hotel));
+    strcpy(temp->name,hotelname);
+    temp->price = price;
+    temp->rooms = rooms;
+    if(Hotel_Head == NULL){
+        temp->next_hotel = NULL;
+        Hotel_Head = temp; 
+    }
+    else{
+        temp->next_hotel = Hotel_Head;
+        Hotel_Head = temp;
+    }
+    return Hotel_Head;
+}
+struct city* addCity(struct city* Head,struct hotel *Hotel_Head, char cityname[]){
+    struct city *temp = (struct city*)malloc(sizeof(struct city));
+    strcpy(temp->name,cityname);
+    if(Head == NULL){
+        temp->next_city = NULL;
+        temp->HOTELS = Hotel_Head;
+        Head = temp;
+    }
+    else{
+        temp->next_city = Head;
+        temp->HOTELS = Hotel_Head;
+        Head = temp;
+    }
+    return Head;
+}
+void readStruct(){
+    City_Head = NULL;
     FILE *F;
     F = fopen("text.txt", "r");
-    char c;
-    char str[100];
-    char hotelname[40];
+    struct city *city_ptr = (struct city *)malloc(sizeof(struct city));
+    char c, city_name[100],hotelname[40];
     double *price = (double *)malloc(sizeof(double));
     int *rooms = (int *)malloc(sizeof(int));
-    while ((c = fgetc(F)) != EOF)
-    {
+    while((c = fgetc(F)) != EOF){
         ungetc(c,F);
-        char s[90];
-        extractCity(F, s);
-        printf("\n\n%s\n", s);
-        while ((c = fgetc(F)) != '#')
-        {
-            ungetc(c,F);
+        extractCity(F,city_name);
+        struct hotel* hotel_head = (struct hotel*)malloc(sizeof(struct hotel));
+        hotel_head = NULL;
+        while((c = fgetc(F)) != '#'){
+            ungetc(c, F);
             extractHotel(F, hotelname, price, rooms);
-            printf("\n%s %lf %d", hotelname, *price, *rooms);
+            hotel_head = addHotel(hotel_head,hotelname, *price, *rooms);
         }
+        City_Head = addCity(City_Head,hotel_head,city_name);
     }
     fclose(F);
+}
+void display(){
+    struct city* temp_c = (struct city*)malloc(sizeof(struct city));
+    struct hotel* hotel_head = (struct hotel*)malloc(sizeof(struct hotel));
+    hotel_head = NULL;
+    temp_c = City_Head;
+    while(temp_c != NULL){
+        printf("\n\n%s\n",temp_c->name);
+        hotel_head = temp_c->HOTELS;
+        while(hotel_head != NULL){
+            printf("Name : %s \t Rooms: %d \t Price : %f\n\n",hotel_head->name,hotel_head->rooms,hotel_head->price);
+            hotel_head = hotel_head->next_hotel;
+        }
+        temp_c = temp_c->next_city;
+    }
+}
+int main()
+{
+    readStruct();
+    display();
     return 0;
 }
