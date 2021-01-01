e1cb27439dfa049cd395bbdbc2e8fdd39ba4e694
DarthMuzammil <muzammil.inamdar98@gmail.com>
2021-01-01 20:13:21 +0530

    FINAL

--- Hotel/admin.c
+++ Hotel/admin.c
@@ -3,25 +3,7 @@
     MUZAMMIL : https://github.com/DarthMuzammil 
 */
 #include "hotel.h"
-#include <sys/ioctl.h> 
-#include <unistd.h> 
-int rowsize(){
-struct winsize size;
-ioctl(STDOUT_FILENO, TIOCGWINSZ, &size);
-return size.ws_row;
-}
-int colsize(){
-    struct winsize size;
-    ioctl(STDOUT_FILENO,TIOCGWINSZ, &size);
-    return size.ws_col;
-}
-void dashes(){
-    printf("\n");
-    for(int i = 0; i < colsize(); i++){
-        printf("-");
-    }
-    printf("\n");
-}
+#include "display.h"
 int main()
 {
     struct city *head = (struct city *)malloc(sizeof(struct city));
